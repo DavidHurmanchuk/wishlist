d99338f38d9cb455398b53d3412aaa327488af2d
David <davidhurmanchuk@gmail.com>
2025-10-24 01:17:41 +0300

    test for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { HashRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 import { WishProvider } from "./context/WishContext";
 
 const App = () => (
-  <WishProvider>
-    <AppRouter />
-  </WishProvider>
+  <HashRouter>
+    <WishProvider>
+      <AppRouter />
+    </WishProvider>
+  </HashRouter>
 );
 
 export default App;
